ef6506569a966454a10bcad9f6dba125b241db59
korichh <akopyankor@gmail.com>
2024-10-29 21:51:02 +0200

    mysql connection check

--- www/index.php
+++ www/index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello World";
+$user = $_ENV["MYSQL_USER"];
+$password = $_ENV["MYSQL_PASSWORD"];
+$host = $_ENV["MYSQL_HOST"];
+$port = $_ENV["MYSQL_PORT"];
+$dbname = $_ENV["MYSQL_NAME"];
+$options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES => false,
+];
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$dbh = new PDO($dsn, $user, $password, $options);
+
+echo "Connected to db!";
